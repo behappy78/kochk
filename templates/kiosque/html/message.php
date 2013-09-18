ae1b268e9b5dd3f40b5091d470b845e94898935c
mehdi.louati@gmail.com <mehdi.louati@gmail.com@ba94d18a-d3e4-c4b3-5c92-f9ad4d078fe9>
2013-09-18 17:14:04 +0000

    mod_menu output Override

--- templates/kiosque/html/message.php
+++ templates/kiosque/html/message.php
@@ -6,7 +6,8 @@
  * @copyright   Copyright (C) 2005 - 2013 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE
  */
-
+// No direct access.
+defined('_JEXEC') or die;
 defined('JPATH_PLATFORM') or die;
 
 /**
