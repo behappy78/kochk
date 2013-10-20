fa37df6bbd22a9e6b57f7c67c97f7b2f415c2c70
mehdi.louati@gmail.com <mehdi.louati@gmail.com@ba94d18a-d3e4-c4b3-5c92-f9ad4d078fe9>
2013-10-20 21:21:59 +0000

    *Change the thumbnails Width size to 232px (Will be the fixed one for media thumbnails) * (+) DB dump for media table

--- trunk/db489062710.sql
+++ trunk/db489062710.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Serveur: localhost
--- Généré le : Dim 20 Octobre 2013 à 02:02
+-- Généré le : Dim 20 Octobre 2013 à 21:19
 -- Version du serveur: 5.1.53
 -- Version de PHP: 5.3.4
 
@@ -59,8 +59,8 @@ CREATE TABLE IF NOT EXISTS `m5jbw_mediamallfactory_media` (
 --
 
 INSERT INTO `m5jbw_mediamallfactory_media` (`id`, `title`, `description`, `rating`, `votes`, `cost_media`, `cost_archive`, `details_media`, `details_archive`, `filename_media`, `filename_archive`, `filename_thumbnail`, `has_media`, `has_archive`, `user_id`, `category_id`, `type_id`, `downloads`, `published`, `created_at`, `updated_at`) VALUES
-(1, 'Media 1', 'Description de Media 1', '0.0', 0, 1, 0, 'Details Media 1', '', '121511P01.pdf', '', 'lapresse-2013-10-19-22-41-10-1.png', 1, 0, 342, 78, 1, 1, 1, '2013-09-09 16:50:13', '2013-10-20 01:56:19'),
-(2, 'N° 19/10/2013', 'INCIDENTS À LA CASERNE DE LA GARDE\r\nNATIONALE D’EL AOUINA\r\nA l’origine, une approche\r\nincohérente', '0.0', 0, 1, 0, 'DETAILS 01', '', '', '', 'lapresse-2013-10-19-22-41-10-2.png', 0, 0, 342, 78, 1, 0, 1, '2013-10-19 21:42:58', '2013-10-20 01:56:31'),
-(3, 'N° 20/10/2013', 'INCIDENTS 2 À LA CASERNE DE LA GARDE\r\nNATIONALE D’EL AOUINA\r\nA l’origine, une approche\r\nincohérente', '0.0', 0, 1, 0, 'DETAILS 02', '', '', '', 'lapresse-2013-10-19-22-41-10-3.png', 0, 0, 342, 78, 1, 0, 1, '2013-10-19 21:48:34', '2013-10-20 01:56:41'),
-(4, 'N° 21/10/2013', 'INCIDENTS À LA CASERNE DE LA GARDE\r\nNATIONALE D’EL AOUINA\r\nA l’origine, une approche\r\nincohérente', '0.0', 0, 1, 0, 'DETAILS 03', '', '', '', 'lapresse-2013-10-19-22-41-10-4.png', 0, 0, 342, 78, 1, 0, 1, '2013-10-19 21:49:30', '2013-10-20 01:56:54'),
-(5, 'N° 22/10/2013', 'INCIDENTS À LA CASERNE DE LA GARDE\r\nNATIONALE D’EL AOUINA\r\nA l’origine, une approche\r\nincohérente', '0.0', 0, 1, 0, 'Details 04', '', '', '', 'lapresse-2013-10-19-22-41-10-5.png', 0, 0, 342, 78, 1, 0, 1, '2013-10-19 21:49:58', '2013-10-20 01:57:06');
+(1, 'Media 1', 'Description de Media 1', '0.0', 0, 1, 0, 'Details Media 1', '', '121511P01.pdf', '', 'lapresse-2013-10-19-22-41-10-1.png', 1, 0, 342, 78, 1, 1, 1, '2013-09-09 16:50:13', '2013-10-20 21:17:59'),
+(2, 'N° 19/10/2013', 'INCIDENTS À LA CASERNE DE LA GARDE\r\nNATIONALE D’EL AOUINA\r\nA l’origine, une approche\r\nincohérente', '0.0', 0, 1, 0, 'DETAILS 01', '', '', '', 'lapresse-2013-10-19-22-41-10-2.png', 0, 0, 342, 78, 1, 0, 1, '2013-10-19 21:42:58', '2013-10-20 21:18:11'),
+(3, 'N° 20/10/2013', 'INCIDENTS 2 À LA CASERNE DE LA GARDE\r\nNATIONALE D’EL AOUINA\r\nA l’origine, une approche\r\nincohérente', '0.0', 0, 1, 0, 'DETAILS 02', '', '', '', 'lapresse-2013-10-19-22-41-10-3.png', 0, 0, 342, 78, 1, 0, 1, '2013-10-19 21:48:34', '2013-10-20 21:18:20'),
+(4, 'N° 21/10/2013', 'INCIDENTS À LA CASERNE DE LA GARDE\r\nNATIONALE D’EL AOUINA\r\nA l’origine, une approche\r\nincohérente', '0.0', 0, 1, 0, 'DETAILS 03', '', '', '', 'lapresse-2013-10-19-22-41-10-4.png', 0, 0, 342, 78, 1, 0, 1, '2013-10-19 21:49:30', '2013-10-20 21:18:28'),
+(5, 'N° 22/10/2013', 'INCIDENTS À LA CASERNE DE LA GARDE\r\nNATIONALE D’EL AOUINA\r\nA l’origine, une approche\r\nincohérente', '0.0', 0, 1, 0, 'Details 04', '', '', '', 'lapresse-2013-10-19-22-41-10-5.png', 0, 0, 342, 78, 1, 0, 1, '2013-10-19 21:49:58', '2013-10-20 21:18:36');
